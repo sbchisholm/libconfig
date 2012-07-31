eb8846884075e6cf8a6574e710ee5cd7104e02f5
Stephen Chisholm <sbchisholm@gmail.com>
2012-07-31 14:47:00 -0300

    working with parsing config into a map

--- config.cpp
+++ config.cpp
@@ -2,6 +2,15 @@
 // Credit: Based off of the mini_xml3.cpp example from the boost spirit 
 //   qi examples. Joel de Guzman: (http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_VARIANT_NO_FULL_RECURSIVE_VARIANT_SUPPORT
+#define BOOST_SPIRIT_DEBUG
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
@@ -11,15 +20,12 @@
 #include <boost/spirit/include/phoenix_object.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/std_pair.hpp>
+
 #include <boost/variant/recursive_variant.hpp>
 #include <boost/variant/get.hpp>
+#include <boost/algorithm/string/join.hpp>
 #include <boost/foreach.hpp>
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
+#include <boost/lexical_cast.hpp>
 
 namespace parse
 {
@@ -48,57 +54,36 @@ namespace parse
     config_tree;
 
     typedef std::map<config_key, config_tree> config_type;
-    typedef config_type::value_type config_pair;
+    typedef std::pair<config_key, config_tree> config_pair;
 
-
-    typedef
-        boost::variant<
-            std::string
-          , double
-          , int
-          , std::vector<std::string>
-          , std::vector<double>
-          , std::vector<int>
-        >
-    config_value;
-
-    typedef 
-        std::pair<std::string, config_value>
-    config_key_value_pair;
-
-    typedef
-        boost::variant<
-            boost::recursive_wrapper<config_struct>
-          , config_key_value_pair
-        >
-    config_item;
-
-    struct config_struct
-    {
-        std::string name;                           // config name
-        std::vector<config_item> children;          // children
-    };
 }
-
-// We need to tell fusion about our config struct
-// to make it a first-class fusion citizen
-BOOST_FUSION_ADAPT_STRUCT(
-    parse::config_struct,
-    (std::string, name)
-    (std::vector<parse::config_item>, children)
-)
 
 namespace parse
 {
     ///////////////////////////////////////////////////////////////////////////
     //  Print out the configuration tree
     ///////////////////////////////////////////////////////////////////////////
-    int const tabsize = 4;
+    int const tabsize = 2;
 
     void tab(int indent)
     {
         for (int i = 0; i < indent; ++i)
             std::cout << ' ';
+    }
+
+    template<typename T>
+    std::string value_to_string(T const& t)
+    {
+      std::ostringstream oss;
+      oss << t;
+      return oss.str();
+    }
+    
+    std::string value_to_string(std::string const& t)
+    {
+      std::ostringstream oss;
+      oss << "\"" << t << "\"";
+      return oss.str();
     }
 
     struct config_printer
@@ -108,28 +93,7 @@ namespace parse
         {
         }
 
-        void operator()(config_struct const& conf) const;
-
-        int indent;
-    };
-
-    struct config_value_to_string : boost::static_visitor<std::string>
-    {
-        template<typename T>
-        std::string operator()(T const& t) const
-        {
-            std::ostringstream oss;
-            oss << t;
-            return oss.str();
-        }
-
-        template<typename T>
-        std::string operator()(std::vector<T> const& t) const
-        {
-            std::ostringstream oss;
-            BOOST_FOREACH(const T& s, t) { oss << s << " "; }
-            return oss.str();
-        }
+        void operator()(config_type const& conf) const;
 
         int indent;
     };
@@ -141,36 +105,43 @@ namespace parse
         {
         }
 
-        void operator()(config_struct const& conf) const
+        template<typename T>
+        void operator()(T const& t) const
         {
-            config_printer(indent+tabsize)(conf);
+            std::cout << " = " << value_to_string(t) << ";" << std::endl;
         }
 
-        void operator()(config_key_value_pair const& pair) const
+        template<typename T>
+        void operator()(std::vector<T> const& t) const
         {
-            tab(indent+tabsize);
-            std::cout << "( "<< pair.first 
-                      << ": " << boost::apply_visitor(config_value_to_string(), pair.second)
-                      << " )" << std::endl;
+            std::cout << " = (";
+            std::vector<std::string> values;
+            BOOST_FOREACH(const T& s, t) { 
+              values.push_back(value_to_string(s)); 
+            }
+            std::cout << boost::algorithm::join(values, ", ")
+                      << ");" << std::endl;
+        }
+
+        void operator()(config_type const& conf) const
+        {
+            std::cout << ": {" << std::endl;
+            config_printer(indent+tabsize)(conf);
+            tab(indent);
+            std::cout << "};" << std::endl;
         }
 
         int indent;
     };
 
-    void config_printer::operator()(config_struct const& conf) const
+    void config_printer::operator()(config_type const& conf) const
     {
-        tab(indent);
-        std::cout << "Section: " << conf.name << std::endl;
-        tab(indent);
-        std::cout << '{' << std::endl;
-
-        BOOST_FOREACH(config_item const& it, conf.children)
+        BOOST_FOREACH(config_type::value_type const& it, conf)
         {
-            boost::apply_visitor(config_item_printer(indent), it);
+            tab(indent);
+            std::cout << it.first;  // Print out the key or section header
+            boost::apply_visitor(config_item_printer(indent), it.second);
         }
-
-        tab(indent);
-        std::cout << '}' << std::endl;
     }
 
     // Define a skipper for white space and comments
@@ -197,7 +168,7 @@ namespace parse
     ///////////////////////////////////////////////////////////////////////////
     template <typename Iterator, typename Skipper = config_skipper<Iterator> >
     struct config_grammar
-      : qi::grammar<Iterator, config_struct(), Skipper>
+      : qi::grammar<Iterator, config_type(), Skipper>
     {
         config_grammar()
           : config_grammar::base_type(config, "config")
@@ -209,7 +180,6 @@ namespace parse
             using qi::lexeme;
             using qi::on_error;
             using qi::fail;
-            using qi::accept;
             using qi::double_;
             using qi::int_;
             using ascii::char_;
@@ -221,17 +191,23 @@ namespace parse
             using phoenix::construct;
             using phoenix::val;
 
-            item %= key_value_pair | config;
+            config %=
+                    *section
+            ;
+
+            item %= 
+                    section | key_value_pair
+            ;
 
             start_tag %=
-                    lexeme[+(alnum | '_')]
+                    key
                 >> !lit('=')
                 >   lit(':') 
                 >   lit('{')
             ;
 
             key_value_pair %=
-                    lexeme[+(alnum | '_')]
+                    key
                 >> !lit(':')
                 >   lit('=')
                 >   ( unesc_str | double_ | int_ | quoted_string_list | double_list | int_list )
@@ -239,11 +215,11 @@ namespace parse
             ;
 
             end_tag =
-                    lit('}') 
+                    lit('}')
                 >   lit(';')
             ;
 
-            config %=
+            section %=
                     start_tag
                 >  *item
                 >   end_tag
@@ -268,6 +244,9 @@ namespace parse
                     ]
                 >   '"'
             ;
+            
+            key %=
+                    lexeme[+(alnum | '_')];
 
             quoted_string %= 
                     unesc_str
@@ -292,7 +271,9 @@ namespace parse
             ;
 
             config.name("config");
+            section.name("section");
             item.name("item");
+            key.name("key");
             key_value_pair.name("key_value_pair");
             quoted_string.name("quoted_string");
             quoted_string_list.name("quoted_string_list");
@@ -312,19 +293,27 @@ namespace parse
                     << val("\"")
                     << std::endl
             );
+
+
+            BOOST_SPIRIT_DEBUG_NODE(key_value_pair);
+            BOOST_SPIRIT_DEBUG_NODE(section);
         }
 
         qi::symbols<char const, char const> unesc_char;
         qi::rule<Iterator, std::string()> unesc_str;
 
-        qi::rule<Iterator, config_struct(), Skipper> config;
-        qi::rule<Iterator, config_item(), Skipper> item;
-        qi::rule<Iterator, config_key_value_pair(), Skipper> key_value_pair;
+        qi::rule<Iterator, config_type(), Skipper> config;
+        qi::rule<Iterator, config_pair(), Skipper> item;
+        qi::rule<Iterator, config_pair(), Skipper> key_value_pair;
+        qi::rule<Iterator, std::pair<config_key, config_type>(), Skipper> section;
+
         qi::rule<Iterator, std::string(), Skipper> quoted_string;
         qi::rule<Iterator, std::vector<std::string>(), Skipper> quoted_string_list;
         qi::rule<Iterator, std::vector<double>(), Skipper> double_list;
         qi::rule<Iterator, std::vector<int>(), Skipper> int_list;
+
         qi::rule<Iterator, std::string(), Skipper> start_tag;
+        qi::rule<Iterator, std::string(), Skipper> key;
         qi::rule<Iterator, void(), Skipper> end_tag;
     };
 }
@@ -365,7 +354,7 @@ int main(int argc, char **argv)
     typedef parse::config_skipper<std::string::const_iterator> config_skipper;
     config_grammar cg; // Our grammar
     config_skipper skipper; // Our skipper
-    parse::config_struct configuration; // Our tree
+    parse::config_type configuration; // Our tree
 
     std::string::const_iterator iter = storage.begin();
     std::string::const_iterator end = storage.end();
